71bcb6ec2c00401b7e7993548f69ecc9a525fb27
Mathew Grimward <mathewgrimward@gmail.com>
2022-01-26 16:00:47 +0000

    Challenge 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,30 @@
 class Oystercard
 
     attr_reader :balance
-  
+
     def initialize(balance = 0)
         @balance = balance
+        @in_journey = false
     end
 
     def top_up(money)
         fail "#{@balance} cannot exceed £90" if (money + @balance) >= 90
-
         @balance += money
+    end
+
+    def deduct(amount)
+        @balance -= amount
+    end
+
+    def in_journey?
+       @in_journey
+    end
+
+    def touch_in
+        @in_journey = true
+    end
+
+    def touch_out
+        @in_journey = false
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,11 +9,31 @@ describe Oystercard do
 
     it '#can be topped up' do
         oystercard.top_up(15)
-
         expect(oystercard.balance).to eq 15
     end
 
     it '#has a max limit of 90' do
         expect{ oystercard.top_up(91) }.to raise_error "#{oystercard.balance} cannot exceed £90"
     end
+
+    it "can deduct balance" do
+        subject.top_up(50)
+        expect{subject.deduct 1}.to change{subject.balance}.by -1
+    end 
+
+    it 'is initially not in a journey' do
+        expect(subject).not_to be_in_journey
+    end
+
+    it "can touch in" do
+        subject.touch_in
+        expect(subject).to be_in_journey
+    end
+
+    it "can touch out" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
+      end
+      
 end
